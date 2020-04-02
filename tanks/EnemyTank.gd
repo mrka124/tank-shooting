ff80564341e0e55ac089ed5d9acda095943c59e6
kietdv <dinhvankiet124@gmail.com>
2020-04-02 23:36:18 +0700

    add code

--- tanks/EnemyTank.gd
+++ tanks/EnemyTank.gd
@@ -1,2 +1,9 @@
 extends "res://tanks/Tank.gd"
 
+onready var parent = get_parent()
+
+func control(delta):
+    if parent is PathFollow2D:
+        parent.set_offset(parent.get_offset() + speed * delta)
+    else:
+        pass
