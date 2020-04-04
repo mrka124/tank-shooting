c07ef7007e107120a059fb6b7619eef3803db27d
kietdv <dinhvankiet124@gmail.com>
2020-04-04 23:15:01 +0700

    update  enemy

--- tanks/EnemyTank.gd
+++ tanks/EnemyTank.gd
@@ -5,28 +5,38 @@ onready var parent = get_parent()
 export (float) var turret_speed
 export (int) var detect_radius
 
+var speed = 0
 var target = null
 
 func _ready():
-    $DetectRadius/CollisionShape2D.shape.radius = detect_radius
+	var circle = CircleShape2D.new()
+	$DetectRadius/CollisionShape2D.shape = circle
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
 
 func control(delta):
-    if parent is PathFollow2D:
-        parent.set_offset(parent.get_offset() + speed * delta)
-        position = Vector2()
-    else:
-        pass
+	if $LookAhead1.is_colliding() or $LookAhead2.is_colliding():
+		speed = lerp(speed, 0, 0.1)
+	else:
+		speed = lerp(speed, max_speed, 0.05)
+
+	if parent is PathFollow2D:
+		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
+	else:
+		pass
 
 func _process(delta):
-    if target:
-        var target_dir = (target.global_position - global_position).normalized()
-        var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)
-        $Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed * delta).angle()
+	if target:
+		var target_dir = (target.global_position - global_position).normalized()
+		var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)
+		$Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed * delta).angle()
+		if target_dir.dot(current_dir) > 0.9:
+			shoot()
 
 func _on_DetectRadius_body_entered(body):
-    if body.name == "Player":
-        target = body
+	if body.name == "Player":
+		target = body
 
 func _on_DetectRadius_body_exited(body):
-    if body == target:
-        target = null
+	if body == target:
+		target = null
